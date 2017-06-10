e142b104f9a39736259da09719b8a95a7d03c51b
TrungTranBao <Baotrungk40pbc@gmail.com>
2017-06-11 00:03:31 +0700

    c

--- GiaoDienCuaHangMoi/GiaoDienCuaHang/frmThongKeNhanVien.cs
+++ GiaoDienCuaHangMoi/GiaoDienCuaHang/frmThongKeNhanVien.cs
@@ -19,7 +19,7 @@ namespace GiaoDienCuaHang
         {
             GiaoDienCuaHang.DataLayer.NhanVienData nv = new GiaoDienCuaHang.DataLayer.NhanVienData();
             DataTable tbl = nv.LayDSNhanVien();
-            CrystalReportNhanVien report = new CrystalReportNhanVien();
+            CRNhanVien report = new CRNhanVien();
             report.SetDataSource(tbl);
             crystalReportViewer1.ReportSource = report;
         }
